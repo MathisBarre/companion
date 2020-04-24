577d9923317f2b98f4b3ef0fa5b74d9f586127b3
Mathis Barré <mathis.barre@live.fr>
2020-04-24 18:11:10 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 class Companion {
 
-  constructor(x = window.innerWidth/2, y = window.innerHeight/2, height = 50, width = 50, bgColor = "grey") {
+  constructor (x, y, height, width, bgColor) {
     // Initialize variables
-    this.height = height
-    this.width = width
+    this.height = height || random(10, 100)
+    this.width = width || this.height || random(10, 100)
+    this.bgColor = bgColor || getRandomColor()
 
     // Create element
     this.elt = document.createElement("div")
 
     this.elt.style.height = this.height + 'px'
     this.elt.style.width = this.width + 'px'
-    this.elt.style.backgroundColor = bgColor
-    this.elt.style.transitionDuration = "1s"
+    this.elt.style.backgroundColor = this.bgColor
+    this.elt.style.transitionDuration = random(1,5) + "s"
 
     this.elt.style.position = 'absolute'
-    this.y = y
-    this.x = x
+    this.y = y || random(0,window.innerHeight)
+    this.x = x || random(0,window.innerWidth)
 
+    this.elt.style.zIndex = "9999999"
     document.getElementsByTagName("body")[0].appendChild(this.elt)
 
+    // Let the companion be alive
     this.beAlive()
   }
 
   beAlive() {
-    const interval = setInterval(() => {
-      const random = Math.random()
-      if(random < 0.25 && random > 0.00) this.up()
-      if(random < 0.50 && random > 0.25) this.down()
-      if(random < 0.75 && random > 0.50) this.left()
-      if(random < 1.00 && random > 0.75) this.right()
-    }, 5 * 1000)
+    setInterval(() => {
+      const randomY = Math.random() * 50 - 25 // Nombre entre -25 et 25
+      const randomX = Math.random() * 50 - 25 // Nombre entre -25 et 25
+      this.x = this._x + randomX
+      this.y = this._y + randomY
+    }, random(1,5) * 1000) // Toutes les 5 secondes
   }
 
   set x(newX) {
@@ -41,22 +43,36 @@ class Companion {
     this._y = newY
     this.elt.style.top = newY + 'px'
   }
-
-  up() {
-    this.y = this._y - 50
-  }
-
-  down() {
-    this.y = this._y + 50
-  }
-
-  left() {
-    this.x = this._x - 50
-  }
-
-  right() {
-    this.x = this._x + 50
-  }
 }
 
-var blocky = new Companion()
+/**
+ * Retourne un nombre entier entre min et max
+ * @param {number} min Nombre minimum
+ * @param {number} max Nombre maximal
+ */
+function random(min, max) {
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
+new Companion()
